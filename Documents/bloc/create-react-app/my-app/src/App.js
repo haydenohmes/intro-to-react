cf983556fba11935420b7267ba6edbfa52263be9
Hayden Ohmes <haydenohmes@gmail.com>
2019-02-12 12:56:41 -0600

    react-to-do-state

---
+++ Documents/bloc/create-react-app/my-app/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import ToDo from './components/ToDo.js';
+
+class App extends Component {
+    constructor(props) {
+     super(props);
+   }
+   this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+  render() {
+    return (
+      <div className="App">
+         <ul>
+            { this.state.todos.map( (todo, index) => 
+            <ToDo key={ index } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
+        </ul>       
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
